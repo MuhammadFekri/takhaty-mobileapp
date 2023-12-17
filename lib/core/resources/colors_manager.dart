ff58e1a23009c81a221cec6d6021b98f507baf22
esamsmesm <smsm22.smesm@gmail.com>
2023-12-18 00:06:18 +0200

    .

--- lib/core/resources/colors_manager.dart
+++ lib/core/resources/colors_manager.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorsManager {
   static const Color primaryColor = Color(0xff2DA13B);
+  static const Color secondary2 = Color(0xff5B998A);
   static const Color blackColor = Color(0xff090C09);
 }
