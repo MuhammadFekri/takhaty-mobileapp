647aa6a36ec2b427a10fbb77cec020bc1b2bd12c
East-Sound <mohab.sarhan17@gmail.com>
2023-12-17 22:30:12 +0200

    add packages

---
+++ lib/features/main_features/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [SvgPicture.asset(""), Text("")],
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
